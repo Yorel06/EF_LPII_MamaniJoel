e0d42fe35ba3993bcbb38b4423ad599bf38804be
Yorel06 <therion959@gmail.com>
2025-02-19 21:19:04 -0500

    creacion de package

---
+++ src/main/java/com/ef/medico/LpiiT3MamanivilcajoelApplication.java
@@ -0,0 +1,13 @@
+package com.ef.medico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpiiT3MamanivilcajoelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpiiT3MamanivilcajoelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ef/medico/LpiiT3MamanivilcajoelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ef.medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpiiT3MamanivilcajoelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
